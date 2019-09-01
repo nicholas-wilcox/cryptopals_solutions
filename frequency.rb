a249485349130c9376c6cd432d11b30e54727d58
Nicholas Wilcox <nicholas.hwilcox@gmail.com>
2019-08-31 18:44:16 -0700

    Add frequency module for challenge 1.3

--- frequency.rb
+++ frequency.rb
@@ -1,47 +1,48 @@
 module Frequency
   module_function
 
-  PRINTABLE_ASCII_RANGE = (0x20..0x7E)
-  ENGLISH_LETTER_FREQUENCY_RANKING = " etaoinshrdlcumwfgypbvkjxqz"
+  LOWERCASE_ASCII_RANGE = (0X61..0x7A)
+  ENGLISH_LETTER_FREQUENCIES = {
+    ?e => 0.12702,
+    ?t => 0.09056,
+    ?a => 0.08167,
+    ?o => 0.07507,
+    ?i => 0.06966,
+    ?n => 0.06749,
+    ?s => 0.06327,
+    ?h => 0.06094,
+    ?r => 0.05987,
+    ?d => 0.04253,
+    ?l => 0.04025,
+    ?c => 0.02782,
+    ?u => 0.02758,
+    ?m => 0.02406,
+    ?w => 0.02360,
+    ?f => 0.02228,
+    ?g => 0.02015,
+    ?y => 0.01974,
+    ?p => 0.01929,
+    ?b => 0.01492,
+    ?v => 0.00978,
+    ?k => 0.00772,
+    ?j => 0.00153,
+    ?x => 0.00150,
+    ?q => 0.00095,
+    ?z => 0.00074
+  }
 
-  def printable?(s)
-    return s.each_byte.all? { |c| PRINTABLE_ASCII_RANGE.cover?(c) }
-  end
-
-  def frequencies(s)
+  def letter_frequencies(s)
     count = Hash.new(0)
-    s.downcase.each_char { |c| count[c] += 1 }
-    return count
-    #return count.each_pair.sort_by { |k, v| v }.map(&:first).join() 
+    letters_of_s = s.downcase.chars.keep_if { |c| LOWERCASE_ASCII_RANGE.include?(c.ord) }
+    letters_of_s.each { |c| count[c] += 1 }
+    return count.transform_values! { |v| v / letters_of_s.length.to_f }
   end
 
   def english_score(s)
-    #freqs = frequencies(s.downcase).keep_if { |k, v| ENGLISH_LETTER_FREQUENCY_RANKING.include? k }
-    #total_letters = freqs.each_pair.map { |k, v| v }.sum
-    return inversions(english_ranking(frequencies(s.downcase)))
-  end
-
-  def english_ranking(freqs)
-    return freqs.keep_if { |k, v| ENGLISH_LETTER_FREQUENCY_RANKING.include? k }.each_pair.sort do |a, b|
-      comp = -(a[1] <=> b[1])
-      if comp.zero?
-        ENGLISH_LETTER_FREQUENCY_RANKING.index(a[0]) <=> ENGLISH_LETTER_FREQUENCY_RANKING.index(b[0])
-      else
-        comp
-      end
-    end.map(&:first).join()
-  end
-
-  def inversions(s)
-    count = 0
-    s.each_char.with_index do |c1, i|
-      s[i+1..-1].each_char.with_index do |c2|
-        if ENGLISH_LETTER_FREQUENCY_RANKING.index(c1) > ENGLISH_LETTER_FREQUENCY_RANKING.index(c2)
-          count += 1
-        end
-      end
-    end
-    return count
+    freqs = letter_frequencies(s)
+    score = 0
+    ENGLISH_LETTER_FREQUENCIES.each { |k, v| score += (freqs[k] - v).abs }
+    return score
   end
 
 end
