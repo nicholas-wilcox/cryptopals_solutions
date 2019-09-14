a7cc18a0bf257ce2423145f1026551509abb37b4
Nicholas Wilcox <nicholas.hwilcox@gmail.com>
2019-09-14 07:46:23 -0700

    syntax improvements

--- hex_string.rb
+++ hex_string.rb
@@ -1,24 +1,23 @@
 class HexString < String
 
-  BASE64_REF = "ABCEDFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"
-  BASE64_PAD = "="
-
   def ^(other)
-    return HexString.new(chars.map.with_index { |c, i| (c.hex ^ other[i].hex).to_s(16) }.join())
+    return HexString.new(chars.map.with_index { |c, i| (c.hex ^ other[i].hex).to_s(16) }.join)
   end
 
   def to_ascii
-    return octets.map { |n| format("%c", n) }.join()
+    return octets.map { |n| format("%c", n) }.join
   end
 
   def xor_against_char(c)
-    return HexString.new(octets.map { |n| n ^ c.ord }.map { |n| n.to_s(16) }.join())
+    return HexString.new(octets.map { |n| n ^ c.ord }.map { |n| n.to_s(16) }.join)
   end
 
-  protected
+  def HexString.from_bytes(s)
+    return HexString.new(s.each_byte.map { |b| format("%02x", b) }.join)
+  end
 
   def octets
-    return chars.each_slice(2).map(&:join).map(&:hex)
+    return (0...length).step(2).map { |i| slice(i, 2) }.map(&:hex)
   end
 
 end
--- vigenere.rb
+++ vigenere.rb
@@ -1,15 +1,14 @@
 require_relative "hex_string"
 
 module Vigenere
-module_function
+  module_function
 
   def xor(s, k)
     return (0..s.length).step(k.length).map { |i| xor_chunk(s[i, k.length], k) }.join
   end
 
   def xor_chunk(s, k)
-    return s.each_byte.map.with_index { |c, i| (c ^ k[i].ord).to_s(16) }
-      .map { |s| (s.length == 1 ? ?0 : '') + s }.join
+    return s.each_byte.map.with_index { |c, i| c ^ k.bytes[i] }.map(&:chr).join
   end
 
 end
