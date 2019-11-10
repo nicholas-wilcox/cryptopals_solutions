991c865315182a9269d90d5543b3849937f92dcb
Nicholas Wilcox <nicholas.hwilcox@gmail.com>
2019-11-10 15:37:20 -0800

    Challenge 10: Use ECB to implement CBC decipher

--- set_2.rb
+++ set_2.rb
@@ -8,5 +8,18 @@ module Set_2
     return (s + (offset.chr * offset)).encode(encoding)
   end
 
+  def challenge10(filename, key, iv=("\x00" * 16))
+    ciphertext = Base64.decode64(File.open(filename, &:read))
+    cipher = OpenSSL::Cipher::AES.new(128, :ECB)
+    cipher.send(:decrypt)
+    cipher.key = key
+    cipher.padding = 0
+
+    return (0...ciphertext.length).step(16).map { |i| ciphertext[i, 16] }.map do |block|
+      plaintext = cipher.update(block).bytes.map.with_index { |c, i| c ^ iv[i].ord }.map(&:chr).join
+      iv = block
+      plaintext
+    end.join + cipher.final
+  end
 
 end
