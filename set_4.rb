27477cab4211e612d2f6448091c1008a42924a68
Nicholas Wilcox <nicholas.hwilcox@gmail.com>
2020-05-04 19:42:52 -0700

    Challenge 25: Refactor ciphertext edit function to not completely decrypt and reencrypt text

--- set_4.rb
+++ set_4.rb
@@ -1,4 +1,6 @@
 require_relative "crypt_util"
+require_relative "string_util"
+require_relative "hex_string"
 
 module Set_4
   module_function
@@ -7,19 +9,26 @@ module Set_4
   def challenge25(plaintext)
     r = Random.new
     key = r.bytes(16)
-
+    # Assuming the nonce is \x00 * 16, it would just be cribbed into the edit function like the key
+    
     ciphertext = CryptUtil.ctr(plaintext, key)
 
-
     edit = ->(ciphertext, key, offset, newtext) do
-      text = CryptUtil.ctr(ciphertext, key)
-      text[offset, newtext.length] = newtext
-      CryptUtil.ctr(text, key)
+      ciphertext.extend(StringUtil)
+      nonce = ("\x00" * 16).extend(StringUtil)
+      q, r = offset.divmod(16)
+      key_stream = q.upto((offset + newtext.length) / 16).map do |i|
+        # Explicitly slice the first 16 bytes, since there's an extra block of encrypted padding
+        CryptUtil.aes_128_ecb(nonce.replace_at((i % 256).chr, 8), key, :encrypt)[0, 16]
+      end.join
+      ciphertext.replace_at(CryptUtil.xor(newtext, key_stream[r, newtext.length]), offset)
     end
 
-    new_ciphertext = edit.call(ciphertext, key, 16, "HAHAHA I changed the plaintext")
+    ciphertext = edit.call(ciphertext, key, 40, "HAHAHA! I changed the fucking plaintext")
 
-    p CryptUtil.ctr(new_ciphertext, key)
+    ciphertext = edit.call(ciphertext, key, 100, "USING THIS HANDY FUNCTION")
+
+    p CryptUtil.ctr(ciphertext, key)
   end
 
 end
