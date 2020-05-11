88a5e2300b52dfdb777487d6b93ecb70598a6921
Nicholas Wilcox <nicholas.hwilcox@gmail.com>
2020-05-10 22:11:32 -0700

    Make encode function strict and remove line breaks

--- utils/base64.rb
+++ utils/base64.rb
@@ -1,10 +1,9 @@
 require_relative 'integer_util'
-require_relative 'string_util'
 
-# This implementation of the Base64 encoding complies with RFC 2045, and should match input and output
-# with Ruby's Base64 module's encode64 and decode64 methods.
-# Notably, the encoding function introduces line breaks after every block of 60 encoded characters, and
-# the decoding function ignores all non valid characters and stops decoding at the first padding character.
+# This implementation of the Base64 encoding features partial compliance with both RFC 2045 and RFC 4648.
+# Notably, the encoding function is strict and does not provide any line breaks. The decoding function
+# ignores all non valid characters and stops decoding at the first padding character.
+# Overall, it should be tested as emulating Ruby's Base64 module's 'strict_encode64' and 'decode64'
 module Utils
   module Base64
     module_function
@@ -13,12 +12,10 @@ module Utils
     PAD = ?=.freeze
 
     def encode(s)
-      s.bytes.each_slice(45).map do |line|
-        line.each_slice(3).map do |block|
-          IntegerUtil.n_tets(6, block.reduce(0) { |mem, n| (mem << 8) + n } << (3 - block.size).*(8), 4)
-            .map(&BASE64_REF.method(:slice))[0, block.size + 1].join
-            .concat(PAD * (3 - block.size))
-        end.join.concat("\n")
+      s.bytes.each_slice(3).map do |block|
+        IntegerUtil.n_tets(6, block.reduce(0) { |mem, n| (mem << 8) + n } << (3 - block.size).*(8), 4)
+          .map(&BASE64_REF.method(:slice))[0, block.size + 1].join
+          .concat(PAD * (3 - block.size))
       end.join
     end
 
