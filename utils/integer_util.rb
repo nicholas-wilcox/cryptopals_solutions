2a0acfa0da4a85273d38c8830d9651ec2f750134
Nicholas Wilcox <nicholas.hwilcox@gmail.com>
2020-05-11 10:03:12 -0700

    Actually fix off by 1 bug

--- utils/integer_util.rb
+++ utils/integer_util.rb
@@ -7,7 +7,7 @@ module Utils
     end
 
     def n_tets(n, m, min_tets = 1)
-      [min_tets, (m.bit_length / n)].max.-(1).downto(0).map { |i| (((2**n - 1) << (i * n)) & m) >> (i * n) }
+      [min_tets, (m.bit_length.to_f / n).ceil].max.-(1).downto(0).map { |i| (((2**n - 1) << (i * n)) & m) >> (i * n) }
     end
 
   end
