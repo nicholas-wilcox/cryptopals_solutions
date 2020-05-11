1cbc49c1b996563cde77d54091e1c30613ae9d12
Nicholas Wilcox <nicholas.hwilcox@gmail.com>
2020-05-10 21:09:09 -0700

    Fix off by 1 bug when number's MSB = 1

--- utils/integer_util.rb
+++ utils/integer_util.rb
@@ -7,7 +7,7 @@ module Utils
     end
 
     def n_tets(n, m, min_tets = 1)
-      [min_tets - 1, (m.bit_length / n)].max.downto(0).map { |i| (((2**n - 1) << (i * n)) & m) >> (i * n) }
+      [min_tets, (m.bit_length / n)].max.-(1).downto(0).map { |i| (((2**n - 1) << (i * n)) & m) >> (i * n) }
     end
 
   end
