5f00bd1c528e48cf04db20d8db02cb2c71a5286a
Nicholas Wilcox <nicholas.hwilcox@gmail.com>
2020-06-04 16:32:17 -0700

    Tidy up literals

--- spec/diffie_hellman_server.rb
+++ spec/diffie_hellman_server.rb
@@ -4,15 +4,16 @@ require_relative '../crypt_util'
 require_relative '../utils'
 
 class DiffieHellmanServer
-  
-  P = ('ffffffffffffffffc90fdaa22168c234c4c6628b80dc1cd129024' +
-       'e088a67cc74020bbea63b139b22514a08798e3404ddef9519b3cd' +
-       '3a431b302b0a6df25f14374fe1356d6d51c245e485b576625e7ec' +
-       '6f44c42e9a637ed6b0bff5cb6f406b7edee386bfb5a899fa5ae9f' +
-       '24117c4b1fe649286651ece45b3dc2007cb8a163bf0598da48361' +
-       'c55d39a69163fa8fd24cf5f83655d23dca3ad961c62f356208552' +
-       'bb9ed529077096966d670c354e4abc9804f1746c08ca237327fff' +
-       'fffffffffffff').hex
+ 
+  # 1536-bit MODP group from https://www.ietf.org/rfc/rfc3526.txt
+  P = ('ffffffffffffffffc90fdaa22168c234c4c6628b80dc1cd1' +
+       '29024e088a67cc74020bbea63b139b22514a08798e3404dd' +
+       'ef9519b3cd3a431b302b0a6df25f14374fe1356d6d51c245' +
+       'e485b576625e7ec6f44c42e9a637ed6b0bff5cb6f406b7ed' +
+       'ee386bfb5a899fa5ae9f24117c4b1fe649286651ece45b3d' +
+       'c2007cb8a163bf0598da48361c55d39a69163fa8fd24cf5f' +
+       '83655d23dca3ad961c62f356208552bb9ed529077096966d' +
+       '670c354e4abc9804f1746c08ca237327ffffffffffffffff').hex
   G = 2
 
   attr_reader :server
