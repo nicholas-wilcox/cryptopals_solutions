917fb2c6c81597deb249ec0be6c53a415f4ef63b
Nicholas Wilcox <nicholas.hwilcox@gmail.com>
2020-06-09 16:26:19 -0700

    Refactor RSA encryption methods to class methods

--- servers/rsa_server.rb
+++ servers/rsa_server.rb
@@ -11,6 +11,14 @@ module Servers
     
     E = 3
     
+    def self.encrypt(plaintext, e, n)
+      Utils::MathUtil.modexp(Utils::HexString.from_bytes(plaintext.bytes).hex, e, n)
+    end
+
+    def self.decrypt(ciphertext, d, n)
+      Utils::MathUtil.modexp(ciphertext, d, n).to_s(16).extend(Utils::HexString).to_ascii
+    end
+    
     def initialize(port, message = '')
       super
       reset_keys
@@ -21,7 +29,7 @@ module Servers
       @p = OpenSSL::BN.generate_prime(bit_size).to_i
       @q = OpenSSL::BN.generate_prime(bit_size).to_i
       @n = @p * @q
-      @private_key = Utils::MathUtil.invmod(3, (@p - 1) * (@q - 1))
+      @private_key = Utils::MathUtil.invmod(E, (@p - 1) * (@q - 1))
     end
 
     def mount
@@ -30,23 +38,15 @@ module Servers
         res.body = { e: E, n: @n }.to_json
       end
 
-      @server.mount_proc('/receiveMessage') { |req| @message = decrypt(req.body.hex, @private_key, @n) }
+      @server.mount_proc('/receiveMessage') { |req| @message = self.class.decrypt(req.body.hex, @private_key, @n) }
 
       @server.mount_proc('/sendMessage') do |req|
         dest_port = JSON.parse(req.body)['port']
         public_key = JSON.parse(get_from('/publicKey', dest_port).body)
-        post_text('/receiveMessage', JSON.parse(req.body)['port'], encrypt(@message, public_key['e'], public_key['n']).to_s(16))
+        post_text('/receiveMessage', JSON.parse(req.body)['port'], self.class.encrypt(@message, public_key['e'], public_key['n']).to_s(16))
       end
     end
 
-    def encrypt(plaintext, e, n)
-      Utils::MathUtil.modexp(Utils::HexString.from_bytes(plaintext.bytes).hex, e, n)
-    end
-
-    def decrypt(ciphertext, d, n)
-      Utils::MathUtil.modexp(ciphertext, d, n).to_s(16).extend(Utils::HexString).to_ascii
-    end
-    
     def send_message_to(serv)
       post_json('/sendMessage', @port, { port: serv.port })
     end
